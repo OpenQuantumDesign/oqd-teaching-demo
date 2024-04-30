b91b4a9c4cd764939d51a17cdd2f058c2a932c6b
yhteoh <yhteoh@uwaterloo.ca>
2024-04-29 20:37:41 -0400

    Added CORS middleware for host FastAPI app

--- pancake/src/host/app.py
+++ pancake/src/host/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pynq.overlays.logictools import LogicToolsOverlay
 
@@ -18,5 +19,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api/pynq", tags=["PYNQ"])
