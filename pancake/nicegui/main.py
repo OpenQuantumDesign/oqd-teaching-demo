b37a4de3aa62518c1fdbe6f99a1e57614e1827e3
Benjamin MacLellan <bmaclell@uwaterloo.ca>
2024-11-25 09:48:07 -0500

    Update GUI

--- pancake/nicegui/main.py
+++ pancake/nicegui/main.py
@@ -8,7 +8,7 @@ sys.path.append("/home/oqd/outreach/")
 from pancake.control.device import Device, RedLasers, BlueLaser, Trap
 from pancake.nicegui.programs import digital_simple
 
-
+from pancake.program import Program
 
 
 class Board:
@@ -31,6 +31,20 @@ class Board:
         return
         # self.device.close()
 
+# class DeviceThread(QThread):
+#     task_done_signal = Signal()
+
+#     def __init__(self, device: Device):
+#         super().__init__()
+#         self.device = device
+
+#     def run(self):
+#         # This will be executed in a separate thread
+#         program = programs['test']  # todo: figure out how to pass in the program object
+#         self.device.run(program=program)
+#         self.task_done_signal.emit()
+
+
 
 def control_card(board: Board):
     with ui.dialog() as control_dialog, ui.card():
@@ -39,9 +53,10 @@ def control_card(board: Board):
         with ui.row().classes('fixed-center'):
 
 
-            with ui.column().classes('w-32'):
+            with ui.list().classes('w-32'):
                 sliders = {}
                 for i in range(5):
+                    ui.label(f"Control Laser {i}")
                     slider = ui.slider(
                         min=0.0, max=1.0, step=0.01, value=0.5, 
                         # on_change=lambda value, idx=i: device.red_lasers_set_intensity(idx=idx, intensity=value)
@@ -69,28 +84,44 @@ def control_card(board: Board):
 
 
 
+# def run(program: Program):
+    # DeviceThread
+
+
 def digital_card(board: Board):
     with ui.dialog() as digital_dialog, ui.card():
-        ui.label('Digital').style('color: #6E93D6; font-size: 200%; font-weight: 300')
+        # ui.label('Digital').style('color: #6E93D6; font-size: 200%; font-weight: 300')
 
 
-        with ui.row().classes('fixed-center'):
+        with ui.column().classes('fixed-center'):
             ui.button('Quantum Circuit', on_click=lambda: board.device.run(digital_simple()))
+
+            ui.button('Stop', on_click=lambda: board.device.stop())
+        
     return digital_dialog
     
 
 def main():
+
     board = Board()
     atexit.register(board.cleanup)
+
+    
 
     control_dialog = control_card(board)
     digital_dialog = digital_card(board)
 
 
-    with ui.row().classes('fixed-center'):
-        ui.button('Control Panel', on_click=control_dialog.open)
-        ui.button('Digital Interface', on_click=digital_dialog.open)
-        ui.button('Analog Interface',)
+
+    # with ui.image('oqd-logo-text.png'):
+    # with ui.image("https://github.com/OpenQuantumDesign/equilux/blob/9ed0c5380133e7d135121c44c3f4cdbcb8cf781b/docs/img/oqd-logo.png?raw=true"):
+    with ui.column():
+        with ui.row().classes('fixed-center'):
+            ui.button('Control Panel', on_click=control_dialog.open)
+            ui.button('Digital Interface', on_click=digital_dialog.open)
+            ui.button('Analog Interface',)
+
+        ui.image("https://github.com/OpenQuantumDesign/equilux/blob/9ed0c5380133e7d135121c44c3f4cdbcb8cf781b/docs/img/oqd-logo.png?raw=true").classes("w-32 h-32")
 
 
 
@@ -102,5 +133,6 @@ if __name__ in {"__main__", "__mp_main__"}:
         host='0.0.0.0',
         port=8080,
         reload=False,
-        # favicon='path_to_favicon.ico',  # Optional favicon
+        # favicon='oqd-logo.png',  # Optional favicon
+        favicon="https://github.com/OpenQuantumDesign/equilux/blob/9ed0c5380133e7d135121c44c3f4cdbcb8cf781b/docs/img/oqd-logo.png?raw=true",
     )
